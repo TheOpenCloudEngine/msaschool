baf6626c8c908464da1d7dab4c5d8fbbf3136716
kimsanghoon1 <sanghoon01@uengine.org>
2020-03-02 17:38:02 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.110e2147059f2b8fff704e8ac043fc0b.js"
+  "/precache-manifest.8d19007ed070ecda0f97c0475b89f5e9.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "msaschool"});
